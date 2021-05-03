1adb2a970a05a5f302b99839ebef1c486f92a0b4
Cale Of <>
2021-05-03 09:46:38 +0000

    initial switches for EVE shifts

--- index.js
+++ index.js
@@ -14,21 +14,30 @@ const moveFile = require('move-file');
 
 var MAINSWITCH = true;
 
-var SHIFT = "GV3"
+var SHIFT = "EV3"
 
 
-var GVY_ON = true
+var GVY_ON = false
 var EVE_ON = true
-var WKNDFRI_ON = true
-var WKND_ON = true
-var EVESUN_ON = true
+var WKNDFRI_ON = false
+var WKND_ON = false
+var EVESUN_ON = false
 
 
-var MOVESPLF_ON = true
-var MOVESPLF_ON = true
+var WPG_ON = false
+
+var CGYMOVE_ON = true
+var WPGMOVE_ON = false
+
+var MOVESPLF_ON = false
+
+var CGY_ON = false
+var VANBBY_ON = false
+var HOMEOFC_ON = false
+var RICHM_ON = false
 
 
-dates.SETANCHORDATE('Mon Apr 19 2021')
+dates.SETANCHORDATE('Mon May 03 2021')
 
 
 
@@ -47,61 +56,81 @@ fs.mkdir(dirname, (err) => {
     console.log("Directory "+dirname+" is created.");
 });
 
+var myPromise = ""
+
+if (EVE_ON) {
+  // EVENING SHIFT
+  SHIFT = shiftcodes.EVE
+  mainhandlerEVE.setDIR(dirname)
+  myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+
+      if (WPG_ON) {
+        var wpg = mainhandlerEVE.WPG_CHECKLISTS()
+        filelist = filelist.concat(wpg); 
+      }
 
 
-/*
-// EVENING SHIFT
-SHIFT = shiftcodes.EVE
-mainhandlerEVE.setDIR(dirname)
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //var wpg = mainhandlerEVE.WPG_CHECKLISTS()
-    //var richh = mainhandlerEVE.RICH_CHECKLISTS()
-    var hofc = mainhandlerEVE.HOFC_CHECKLISTS()  
-    //var wpgMOVE = mainhandlerEVE.WPG_MOVEALLS()
-    //var cgyMOVE = mainhandlerEVE.CGY_MOVEALLS()  
+      if (RICHM_ON) {
+        var richh = mainhandlerEVE.RICH_CHECKLISTS()
+        filelist = filelist.concat(richh);
+      }
 
-    //filelist = filelist.concat(wpg);  
-    //filelist = filelist.concat(richh);
-    filelist = filelist.concat(hofc); 
-    //filelist = filelist.concat(wpgMOVE); 
-    //filelist = filelist.concat(cgyMOVE);            
-    resolve(filelist)
- 
-  }, 49999);
-});
-*/
+       
+      if (HOMEOFC_ON) {
+        var hofc = mainhandlerEVE.HOFC_CHECKLISTS()  
+        filelist = filelist.concat(hofc); 
+      }
+     
+      if (WPGMOVE_ON) {
+        var wpgMOVE = mainhandlerEVE.WPG_MOVEALLS()
+        filelist = filelist.concat(wpgMOVE); 
+      }
 
+      if (CGYMOVE_ON) {
+        var cgyMOVE = mainhandlerEVE.CGY_MOVEALLS()  
+        filelist = filelist.concat(cgyMOVE);   
+      }
 
-
-// GRAVEYARD SHIFT
-SHIFT = shiftcodes.GVY
-mainhandlerGVY.setDIR(dirname)
-const myPromise = new Promise((resolve, reject) => {
-  setTimeout(() => {
-    //var sploutqs = mainhandlerGVY.CREATEALL_MVALLSPLOUTQS()
-
-    var cgy = mainhandlerGVY.CGY_CHECKLISTS()
-    //var vb = mainhandlerGVY.VANBBY_CHECKLISTS() 
-    var inp = ["mon"]
-    //var scd = mainhandlerGVY.SCD_VBCGY(inp)
-
-    //var richh = mainhandlerGVY.RICHHOFC_FULLCHECKLISTS()
-    //var hofc = mainhandlerGVY.HOFC_FULLCHECKLISTS()
-    //var vdivs = mainhandlerGVY.VANDIVS_FULLCHECKLISTS()
-  
-    //filelist = filelist.concat(sploutqs);  
-    filelist = filelist.concat(cgy);
-    //filelist = filelist.concat(vb);
-    //filelist = filelist.concat(scd);    
-    //filelist = filelist.concat(richh);
-    //filelist = filelist.concat(hofc);
-    //filelist = filelist.concat(vdivs);
-
+    
+         
       resolve(filelist)
- 
-  }, 49999);
-});
+  
+    }, 49999);
+  });
+}
+
+
+if (GVY_ON) {
+  // GRAVEYARD SHIFT
+  SHIFT = shiftcodes.GVY
+  mainhandlerGVY.setDIR(dirname)
+  const myPromise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      //var sploutqs = mainhandlerGVY.CREATEALL_MVALLSPLOUTQS()
+
+      //var cgy = mainhandlerGVY.CGY_CHECKLISTS()
+      //var vb = mainhandlerGVY.VANBBY_CHECKLISTS() 
+      var inp = ["mon"]
+      //var scd = mainhandlerGVY.SCD_VBCGY(inp)
+
+      //var richh = mainhandlerGVY.RICHHOFC_FULLCHECKLISTS()
+      var hofc = mainhandlerGVY.HOFC_FULLCHECKLISTS()
+      var vdivs = mainhandlerGVY.VANDIVS_FULLCHECKLISTS()
+    
+      //filelist = filelist.concat(sploutqs);  
+      //filelist = filelist.concat(cgy);
+      //filelist = filelist.concat(vb);
+      //filelist = filelist.concat(scd);    
+      //filelist = filelist.concat(richh);
+      filelist = filelist.concat(hofc);
+      filelist = filelist.concat(vdivs);
+
+        resolve(filelist)
+  
+    }, 49999);
+  });
+}
 
 
 
